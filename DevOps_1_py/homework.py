7908f370cc8d594cdb000942e8a1536e54361a49
Mykhailo Rozdorozhniuk <misha.roz.2003@gmail.com>
2025-08-22 16:49:15 +0200

    feat(hw 1): rewright code with functions

--- DevOps_1_py/homework.py
+++ DevOps_1_py/homework.py
@@ -1,20 +1,25 @@
 import random
 
-random_generated_number = random.randint(0, 100)
-ATTEMPTS = 5
-local_attempt = 0
+def play_guessing_game():
+    random_generated_number = random.randint(1, 100)
+    ATTEMPTS = 5
 
-while local_attempt < ATTEMPTS:
-    guess_number = int(input(f"Attempt {local_attempt + 1}/{ATTEMPTS}. Enter your guess: "))
+    for local_attempt in range(ATTEMPTS):
+        try:
+            guess_number = int(input(f"Attempt {local_attempt + 1}/{ATTEMPTS}. Enter your guess: "))
 
-    if guess_number == random_generated_number:
-        print("Congratulations")
-        break
-    elif guess_number < random_generated_number:
-        print("try higher")
-    else:
-        print("try smaller")
+            if guess_number == random_generated_number:
+                print("Congratulations! You guessed the right number.")
+                return
+            elif guess_number < random_generated_number:
+                print("Too low. Try a higher number.")
+            else:
+                print("Too high. Try a smaller number.")
 
-    local_attempt += 1
-else:
-    print(f"Sorry, you've used all attempts. The number was {random_generated_number}")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+    print(f"Sorry, you've run out of attempts. The correct number was {random_generated_number}.")
+
+if __name__ == "__main__":
+    play_guessing_game()
